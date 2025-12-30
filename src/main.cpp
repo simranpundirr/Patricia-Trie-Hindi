5bd2455001a1d60fe9bb3e2c27238e11dad75131
simranpundirr <122039675+simranpundirr@users.noreply.github.com>
2025-12-30 18:46:02 +0530

    Add main file to test standard trie

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    Trie trie;
+    vector<string> words = {
+        "राम",
+        "रामायण",
+        "रामकथा",
+        "राज",
+        "राजा",
+        "राज्य",
+        "रानी",
+        "राष्ट्र"
+    };
+
+    for (const auto &word : words) {
+        trie.insert(word);
+    }
+    cout << trie.search("राम") << endl;
+    cout << trie.search("राज") << endl;
+    cout << trie.search("राजकुमार") << endl;
+
+    cout << "Standard Trie Node Count: "
+         << trie.countNodes() << endl;
+
+    return 0;
+}
